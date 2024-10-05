e1a9e6710e42443f4f0cd8e13fae8beec7e42eb6
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-10-05 18:54:16 +0300

    v0.1 some tests are failing for rows

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const tbody = table.querySelector('tbody');
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+// const appendColumnButton = document.querySelector('.append-column');
+// const removeColumnButton = document.querySelector('.remove-column');
+
+appendRowButton.addEventListener('click', () => {
+  const columns = table.querySelector('tr').children.length;
+  const newRow = document.createElement('tr');
+
+  for (let i = 0; i < columns; i++) {
+    const newTd = document.createElement('td');
+
+    newRow.appendChild(newTd);
+  }
+
+  tbody.appendChild(newRow);
+
+  disableButtons();
+  enableButtons();
+});
+
+removeRowButton.addEventListener('click', () => {
+  tbody.removeChild(tbody.lastElementChild);
+
+  disableButtons();
+  enableButtons();
+});
+
+function disableButtons() {
+  const rows = tbody.children.length;
+
+  if (rows <= 2) {
+    removeRowButton.disabled = true;
+  }
+
+  if (rows >= 10) {
+    appendRowButton.disabled = true;
+  }
+}
+
+function enableButtons() {
+  const rows = tbody.children.length;
+
+  if (rows > 2) {
+    removeRowButton.disabled = false;
+  }
+
+  if (rows < 10) {
+    appendRowButton.disabled = false;
+  }
+}
